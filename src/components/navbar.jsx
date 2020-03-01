443098a63c5e3da117b4e6f9f00e26762b4be542
akki <akshat@shopinsync.com>
2020-03-02 00:51:31 +0530

    website logo change

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,7 @@ export default class Navbar extends Component{
                     <nav className="navbar navbar-expand-lg navbar-light">
                         <div className="container">
                             {/*Brand and toggle get grouped for better mobile display*/}
-                            <a className="navbar-brand logo_h" href="index.html"><img src="img/logo.png" alt=""></img></a>
+                            <a className="navbar-brand logo_h" href="/"><img src="img/logo.png" alt=""></img></a>
                             <button className="navbar-toggler" type="button" data-toggle="collapse"
                                     data-target="#navbarSupportedContent"
                                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,7 +19,7 @@ export default class Navbar extends Component{
                             {/*Collect the nav links, forms, and other content for toggling*/}
                             <div className="collapse navbar-collapse offset" id="navbarSupportedContent">
                                 <ul className="nav navbar-nav menu_nav justify-content-end">
-                                    <li className="nav-item active"><a className="nav-link" href="index.html">Home</a></li>
+                                    <li className="nav-item active"><a className="nav-link" href="/">Home</a></li>
                                     {/*<li className="nav-item"><a className="nav-link" href="/Skills">Blog</a></li>*/}
                                 </ul>
                             </div>
